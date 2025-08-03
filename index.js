8b549bd1dbac910ce5ddbee240307d1c8a2e9177
Elon Amram <elonamrammm1234@gmail.com>
2025-08-03 18:15:45 +0300

    started work with require express,path,dotenv

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 //npm install express ,ejs ,dotenv, mysql2
+
+const express = require('express');
+const path = require("path");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// הגדרות תצוגה
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// קבצים סטטיים
+app.use(express.static(path.join(__dirname, "public")));
+
+// תמיכה ב-POST
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// // דוגמה למסך בית
+// app.get("/", (req, res) => {
+//     res.render("dashboard", { user: "אורח" });
+// });
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
