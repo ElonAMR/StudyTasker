dabf90b0d5feb234008b829258a2016599ba161e
Elon Amram <elonamrammm1234@gmail.com>
2025-08-06 02:04:51 +0300

    create Middleware for CheckLogin with protected from sql injection and add salt to password

---
+++ Middleware/user_Mid.js
@@ -0,0 +1,30 @@
+const md5 = require('md5');
+
+async function CheckLogin(req, res,next) {
+    let uname   = (req.body.uname  !== undefined) ? addSlashes(req.body.uname     ) : "";
+    let passwd  = (req.body.passwd !== undefined) ?            req.body.passwd      : "";
+    let enc_pass = md5("A"+passwd);
+    let Query = `SELECT * FROM users WHERE uname = '${uname}' AND passwd = '${enc_pass}'`;
+
+    const promisePool = db_pool.promise();
+    try {
+        const [rows] = await promisePool.query(Query);
+
+        if (rows.length > 0) {
+            req.validUser = true;
+            req.user = rows[0];
+        } else {
+            req.validUser = false;
+        }
+
+    } catch (err) {
+        console.log("שגיאה בבדיקת התחברות:", err);
+        req.validUser = false;
+    }
+    next();
+}
+
+
+module.exports = {
+    CheckLogin,
+}
