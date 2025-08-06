35ec78a470e9ea2d8d51d8d7011f996e3a32ed78
Elon Amram <elonamrammm1234@gmail.com>
2025-08-07 01:29:09 +0300

    Added Middleware with empty Function for structure

---
+++ Middleware/category_Mid.js
@@ -0,0 +1,32 @@
+// Middleware להוספת קטגוריה
+async function AddCategory(req, res, next) {
+
+    next();
+}
+
+
+
+async function EditCategory(req, res, next) {
+
+    next();
+}
+
+async function ShowCategory(req, res, next) {
+
+
+    next();
+}
+
+
+async function DeleteCategory(req, res, next) {
+
+    next();
+}
+
+
+module.exports = {
+    AddCategory,
+    EditCategory,
+    ShowCategory,
+    DeleteCategory
+};
