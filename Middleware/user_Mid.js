da88dd6f8c2d509e2aa1154f186f9d931eabf4a4
Elon Amram <elonamrammm1234@gmail.com>
2025-08-07 17:13:47 +0300

    added function IsLogged check if user connect

--- Middleware/user_Mid.js
+++ Middleware/user_Mid.js
@@ -1,5 +1,27 @@
 const md5 = require('md5');
 
+async function isLogged(req, res,next){
+    const jwtToken = req.cookies.ImLoggedToYoman;
+    let user_id=-1;
+    if (jwtToken !== "") {
+        jwt.verify(jwtToken, 'myPrivateKey', async (err, decodedToken) => {
+            if (err) {
+                console.log("err=",err);
+            } else {
+                let data = decodedToken.data;
+                user_id = data.split(",")[0];
+                req.user_id=user_id;
+            }
+        })
+    }
+
+    if(user_id < 0)
+        res.redirect("/");
+
+    next();
+}
+
+
 async function CheckLogin(req, res,next) {
     let uname   = (req.body.uname  !== undefined) ? addSlashes(req.body.uname     ) : "";
     let passwd  = (req.body.passwd !== undefined) ?            req.body.passwd      : "";
@@ -7,24 +29,30 @@ async function CheckLogin(req, res,next) {
     let Query = `SELECT * FROM users WHERE uname = '${uname}' AND passwd = '${enc_pass}'`;
 
     const promisePool = db_pool.promise();
+    let rows=[];
     try {
-        const [rows] = await promisePool.query(Query); // objects in arr and result from Query in index [0]
-
-        if (rows.length > 0) {
-            req.validUser = true;
-            req.user = rows[0];
-        } else {
-            req.validUser = false;
-        }
-
+        [rows] = await promisePool.query(Query);
     } catch (err) {
-        console.log("שגיאה בבדיקת התחברות:", err);
-        req.validUser = false;
+        console.log(err);
+    }
+
+    if(rows.length > 0){
+        req.validUser = true;
+        let val = `${rows[0].id},${rows[0].name}`;
+        let token = jwt.sign(
+            {data: val},
+            'myPrivateKey',
+            { expiresIn: 31*24*60*60 // in sec
+            });
+        res.cookie("ImLoggedToYoman", token, {
+            maxAge: 31*24*60*60 * 1000, // 3hrs in ms
+        });
     }
     next();
 }
 
 
 module.exports = {
-    CheckLogin,
+    isLogged,
+    CheckLogin
 };
